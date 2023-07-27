b2ba5ceddb80ec073e5835579c9cfac73b1dce18
Gregory Thompson <greg@thegregthompson.com>
2023-07-27 10:29:24 -0400

    new layout updates

--- app/components/StatusSection.tsx
+++ app/components/StatusSection.tsx
@@ -1,9 +1,23 @@
 import React, { useEffect, useState, useRef } from "react";
 import useResize from "hooks/useResize";
-import {DateTime} from "luxon";
-import { animated, useSpring } from '@react-spring/web'
+import { DateTime } from "luxon";
+import { animated, useSpring } from "@react-spring/web";
 
-export default function StatusSection({orders, statusKey, statusOptions, color=''}: {section: String, color: String}): React.ReactElement {
+export default function StatusSection({
+  orders,
+  statusKey,
+  statusOptions,
+  color = "",
+  fullHeight,
+  titleOverride,
+  isCustom,
+}: {
+  section: String;
+  color: String;
+  fullHeight: Boolean;
+  titleOverride: String;
+  isCustom: Boolean;
+}): React.ReactElement {
   const resize = useResize();
   const refContainer = useRef(null);
   const [isLargerThanContainer, setIsLargerThanContainer] = useState(false);
@@ -16,17 +30,17 @@ export default function StatusSection({orders, statusKey, statusOptions, color='
       y: 0,
     }),
     []
-  )
-  
+  );
+
   useEffect(() => {
-    if(resize.length > 0){
+    if (resize.length > 0) {
       const containerHeight = refContainer.current.offsetHeight - 45;
       const dataHeight = dataRef.current.clientHeight;
       setAnimationConfig({
         dataHeight,
         containerHeight,
       });
-      if(dataHeight > containerHeight){
+      if (dataHeight > containerHeight) {
         setIsLargerThanContainer(true);
       }
     }
@@ -39,116 +53,207 @@ export default function StatusSection({orders, statusKey, statusOptions, color='
       dataHeight,
       containerHeight,
     });
-      
-    if(dataHeight > containerHeight){
+
+    if (dataHeight > containerHeight) {
       setIsLargerThanContainer(true);
-    } else{
+    } else {
       setIsLargerThanContainer(false);
     }
   }, [orders]);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if(isLargerThanContainer){
-        const {dataHeight, containerHeight} = animationConfig;
+      if (isLargerThanContainer) {
+        const { dataHeight, containerHeight } = animationConfig;
         api.start({
           config: {
-            duration: 10000
+            duration: 10000,
           },
           from: { y: 0 },
-          to: { y: ((dataHeight-containerHeight)*-1)-25},
-        })
+          to: { y: (dataHeight - containerHeight) * -1 - 25 },
+        });
 
         api.start({
           config: {
-            duration: 10000
+            duration: 10000,
           },
           delay: 20000,
-          from: { y: ((dataHeight-containerHeight)*-1)-25 },
+          from: { y: (dataHeight - containerHeight) * -1 - 25 },
           to: { y: 0 },
-        })
+        });
       }
-    }, 35000);  
+    }, 35000);
     return () => clearInterval(interval);
   }, [api, isLargerThanContainer]);
 
-  
   const filteredOrders = orders.filter((order) => {
     return order.status_value.status_option.key == statusKey;
   });
 
+  const isFullHeight = fullHeight ? "h-screen" : "";
+
   return (
-    <div className={`w-full flex flex-col ${color}`} ref={refContainer}>
-      <div className="text-center text-lg py-2 font-bold header">{statusOptions.find((option)=>option.key == statusKey).name} ({filteredOrders.length})</div>
-      <div className="flex flex-col py-2 px-3 flex-1 overflow-hidden relative">
-      <animated.div
-      style={{
-        ...styles
-      }}
-      
-      >
-        <div className={isLargerThanContainer ? 'marquee' : ''} ref={dataRef}>
-          {filteredOrders.map((order)=> {
-            
-            const modelInfo = JSON.parse(order.quote[0].model_info);
-            const details = JSON.parse(order.quote[0].details);
-            const statusDate = DateTime.fromSQL(order.status_value.created_at);
-            const now = DateTime.now();
-            const diff = Math.ceil(now.diff(statusDate, ['days']).toObject().days);
-            let background = 'bg-white';
-            let orderIdColor = 'text-black';
-            switch(diff){
-              case 0:
-                background = 'bg-white';
-                break;
-              case 1:
-                background = 'bg-white';
-                break;
-              case 2:
-                background = 'bg-yellow-100';
-                orderIdColor = 'text-yellow-800';
-                break;
-              case 3:
-                background = 'bg-orange-100';
-                orderIdColor = 'text-orange-800';
-                break
-              default:
-                background = 'bg-red-100';
-                orderIdColor = 'text-red-800';
-                break;
-
-            }
-            let orderString = order.quote[0].model_desc;
-            if(orderString.length > 50){
-              orderString = order.quote[0].model_desc.substr(0, 50) + "\u2026"
-            }
-            return (
-            <React.Fragment key={order.id}>
-              {order.status_value.status_option.key == statusKey && (
-                <div className={`${background} py-0.5 px-2 rounded shadow-sm mb-1 flex justify-between`}>
-                  <div>
-                  <span className={`${orderIdColor} font-bold text-sm inline-block`}>{order.id}</span> &#x2022; <span className="text-sm">{orderString}</span><br />
-                  </div>
-                  <span>
-                    {modelInfo.working_status == 'working' ? (
-                      <span className="inline-flex items-center rounded-full bg-emerald-100 px-2.5 py-0.5 text-xs font-medium text-emerald-800">Working</span>
-                    ):(
-                      <span className="inline-flex items-center rounded-full bg-red-100 px-2.5 py-0.5 text-xs font-medium text-red-800">Broken</span>
+    <div
+      className={`flex w-full flex-col ${color} ${isFullHeight}`}
+      ref={refContainer}
+    >
+      {!titleOverride ? (
+        <div className="header py-2 text-center text-lg font-bold">
+          {statusOptions.find((option) => option.key == statusKey).name} (
+          {filteredOrders.length})
+        </div>
+      ) : (
+        <div className="header py-2 text-center text-lg font-bold">
+          {titleOverride}
+        </div>
+      )}
+      <div className="relative flex flex-1 flex-col overflow-hidden py-2 px-3">
+        <animated.div
+          style={{
+            ...styles,
+          }}
+        >
+          {!isCustom ? (
+            <div
+              className={isLargerThanContainer ? "marquee" : ""}
+              ref={dataRef}
+            >
+              {filteredOrders.map((order) => {
+                const modelInfo = JSON.parse(order.model_info);
+                const details = JSON.parse(order.details);
+                const isBulk = order.cart.bulk_order != null;
+                let orderDetails = order.cart.order;
+                if (isBulk) {
+                  orderDetails = order.cart.bulk_order;
+                }
+                const statusDate = DateTime.fromSQL(
+                  order.status_value.created_at
+                );
+                const now = DateTime.now();
+                const diff = Math.ceil(
+                  now.diff(statusDate, ["days"]).toObject().days
+                );
+                let background = "bg-white";
+                let orderIdColor = "text-black";
+                switch (diff) {
+                  case 0:
+                    background = "bg-white";
+                    break;
+                  case 1:
+                    background = "bg-white";
+                    break;
+                  case 2:
+                    background = "bg-yellow-100";
+                    orderIdColor = "text-yellow-800";
+                    break;
+                  case 3:
+                    background = "bg-orange-100";
+                    orderIdColor = "text-orange-800";
+                    break;
+                  default:
+                    background = "bg-red-100";
+                    orderIdColor = "text-red-800";
+                    break;
+                }
+                let orderString = order.model_desc;
+                if (orderString.length > 50) {
+                  orderString = order.model_desc.substr(0, 50) + "\u2026";
+                }
+                return (
+                  <React.Fragment key={order.id}>
+                    {order.status_value.status_option.key == statusKey && (
+                      <div
+                        className={`${background} mb-1 flex justify-between rounded py-0.5 px-2 shadow-sm`}
+                      >
+                        <div>
+                          <span>
+                            {modelInfo.working_status == "working" ? (
+                              <span
+                                className="mr-1 inline-flex h-2 w-2 items-center rounded-full bg-emerald-800 text-xs font-medium"
+                                style={{ marginBottom: "1px" }}
+                              ></span>
+                            ) : (
+                              <span
+                                className="mr-1 inline-flex h-2 w-2 items-center rounded-full bg-red-800 text-xs font-medium"
+                                style={{ marginBottom: "1px" }}
+                              ></span>
+                            )}
+                          </span>
+                          <span
+                            className={`${orderIdColor} inline-block text-sm font-bold`}
+                          >
+                            {order.id}
+                          </span>{" "}
+                          &#x2022;{" "}
+                          <span className="text-sm">{orderString}</span>
+                          <br />
+                        </div>
+                        <span>
+                          {isBulk ? (
+                            <span className="inline-flex items-center whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs font-medium text-purple-800">
+                              {/* {orderDetails.company ? (
+                                <>{orderDetails.company}</>
+                              ) : (
+                                <>
+                                  {orderDetails.first_name}{" "}
+                                  {orderDetails.last_name}
+                                </>
+                              )} */}
+                              {orderDetails.id}
+                            </span>
+                          ) : (
+                            <span className="inline-flex items-center whitespace-nowrap rounded-full bg-orange-100 px-2.5 py-0.5 text-xs font-medium text-orange-800">
+                              {/* {orderDetails.first_name} {orderDetails.last_name} */}
+                              {orderDetails.id}
+                            </span>
+                          )}
+                        </span>
+                      </div>
                     )}
-                  </span>
-                  
-                </div>
-    
-
-              )}
-            </React.Fragment>
-          )})}
-          </div> 
-          </animated.div>
+                  </React.Fragment>
+                );
+              })}
+            </div>
+          ) : (
+            <div
+              className={isLargerThanContainer ? "marquee" : ""}
+              ref={dataRef}
+            >
+              {orders.map((order) => {
+                return (
+                  <React.Fragment key={order.id}>
+                    <div
+                      className={`mb-1 flex justify-between rounded bg-white py-0.5 px-2 shadow-sm`}
+                    >
+                      <div>
+                        <span
+                          className={`$text-black inline-block text-sm font-bold`}
+                        >
+                          {order.order_id}-{order.id}
+                        </span>{" "}
+                        &#x2022;{" "}
+                        <span className="text-sm">{order.description}</span>
+                        <br />
+                      </div>
+                      <span>
+                        <span className="inline-flex items-center whitespace-nowrap rounded-full bg-blue-100 px-2.5 py-0.5 text-xs font-medium text-blue-800">
+                          {order.custom.company ? (
+                            <>{order.custom.company}</>
+                          ) : (
+                            <>
+                              {order.custom.first_name} {order.custom.last_name}
+                            </>
+                          )}
+                        </span>
+                      </span>
+                    </div>
+                  </React.Fragment>
+                );
+              })}
+            </div>
+          )}
+        </animated.div>
       </div>
-    
     </div>
   );
 }
-
-
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,47 +1,43 @@
-import React, {useState, useCallback, useEffect} from 'react';
+import React, { useState, useCallback, useEffect } from "react";
 import type { LoaderArgs } from "@remix-run/node";
 import StatusSection from "~/components/StatusSection";
 import { useOptionalUser } from "~/utils";
 import { useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/node";
-import stylesheetUrl from '../styles/global.css';
+import stylesheetUrl from "../styles/global.css";
 import { getOrders } from "~/models/orders.server";
-import Pusher from 'pusher-js';
+import Pusher from "pusher-js";
 import sortBy from "lodash/sortBy";
 import indexOf from "lodash/indexOf";
-import { useInterval } from 'usehooks-ts'
-
-
+import { useInterval } from "usehooks-ts";
 
 export function links() {
   // `links` returns an array of objects whose
   // properties map to the `<link />` component props
-  return [
-    { rel: "stylesheet", href: stylesheetUrl },
-  ];
+  return [{ rel: "stylesheet", href: stylesheetUrl }];
 }
 
-export async function loader ({ request }: LoaderArgs) {
+export async function loader({ request }: LoaderArgs) {
   return json(await getOrders());
-};
-
+}
 
 export default function Index() {
-  const {data, status_options} = useLoaderData();
+  const { data, custom, status_options } = useLoaderData();
   const [orders, setOrders] = useState(data);
+  const [customOrders, setCustomOrders] = useState(custom);
   const [channel, setChannel] = useState(null);
   const [shouldReset, setShouldReset] = useState(true);
   const [pusher, setPusher] = useState(null);
 
   useEffect(() => {
-    if(shouldReset){
-      const pusherInstance = new Pusher('8dbf7fe9fc3eebec3913', {
-        cluster: 'us2',
+    if (shouldReset) {
+      const pusherInstance = new Pusher("8dbf7fe9fc3eebec3913", {
+        cluster: "us2",
       });
-      pusherInstance.connection.bind( 'error', function( err ) {
-        if( err.error.data.code === 4004 ) {
-          alert('Over limit!');
-        } else{
+      pusherInstance.connection.bind("error", function (err) {
+        if (err.error.data.code === 4004) {
+          alert("Over limit!");
+        } else {
           alert(`Pusher Error: ${err.error.data.code}`);
         }
       });
@@ -51,46 +47,49 @@ export default function Index() {
   }, [shouldReset]);
 
   useEffect(() => {
-    if(pusher && !channel){
-      console.log("============================SUBSCRIBING TO CHANNEL============================");
-      
-      setChannel(pusher.subscribe("orders"));
+    if (pusher && !channel) {
+      console.log(
+        "============================SUBSCRIBING TO CHANNEL============================"
+      );
+
+      setChannel(pusher.subscribe("quotes"));
 
       console.log(pusher);
 
       pusher.connection.bind("connected", function () {
-        console.log('Pusher Connected')
+        console.log("Pusher Connected");
       });
       pusher.connection.bind("error", function (error) {
         console.error("connection error", error);
       });
       pusher.connection.bind("state_change", function (states) {
-        console.log('Pusher state change', states);
+        console.log("Pusher state change", states);
       });
       pusher.connection.bind("disconnected", function () {
-        console.log('Pusher Disconnected')
+        console.log("Pusher Disconnected");
       });
       pusher.connection.bind("failed", function () {
-        console.log('Pusher failed')
+        console.log("Pusher failed");
       });
-      
     }
     return () => {
-      if(channel && pusher && pusher.connection.state !== 'disconnected') 
-      {
-        console.log("============================UNSUBSCRIBING FROM CHANNEL============================");
+      if (channel && pusher && pusher.connection.state !== "disconnected") {
+        console.log(
+          "============================UNSUBSCRIBING FROM CHANNEL============================"
+        );
         channel.unbind();
-        pusher.unsubscribe("orders");
+        pusher.unsubscribe("quotes");
       }
-    }
+    };
   }, [channel, pusher]);
 
   const rebuildSubscripton = useCallback(() => {
-    if(channel && pusher)
-    {
-      console.log("============================REBUILDING SUBSCRIPTION============================");
+    if (channel && pusher) {
+      console.log(
+        "============================REBUILDING SUBSCRIPTION============================"
+      );
       channel.unbind();
-      pusher.unsubscribe("orders");
+      pusher.unsubscribe("quotes");
       pusher.disconnect();
       setChannel(null);
       setPusher(null);
@@ -98,18 +97,22 @@ export default function Index() {
     }
   }, [channel, pusher]);
 
-  useInterval(
-    () => {
-      const state = pusher.connection.state;
-      console.log('Pusher state', state);
-      // rebuildSubscripton()
-    },
-    5000
-  )
+  useInterval(() => {
+    const state = pusher.connection.state;
+    console.log("Pusher state", state);
+    // rebuildSubscripton()
+  }, 5000);
 
-  
+  useInterval(async () => {
+    const response = await fetch(
+      `http://goroostr-api.test/api/get-status-orders`
+    );
+    const { data, custom } = await response.json();
+    // setOrders(orders);
+    setCustomOrders(custom);
+  }, 300000);
 
-  const pluck = property => element => element[property];
+  const pluck = (property) => (element) => element[property];
 
   const sortAndOrder = useCallback((data, statusOptionsDirect) => {
     data.forEach((order, i) => {
@@ -117,7 +120,7 @@ export default function Index() {
     });
     statusOptionsDirect.sort((a, b) => a.order - b.order);
     const keys = statusOptionsDirect.map(pluck("key"));
-    const sorted = sortBy(data, function(obj){
+    const sorted = sortBy(data, function (obj) {
       return indexOf(keys, obj.statusKey);
     });
 
@@ -125,46 +128,89 @@ export default function Index() {
   }, []);
 
   useEffect(() => {
-    if(!channel) return;
-    channel.bind(`orders.update`, (response) => {
-      console.log('============================ORDER UPDATE============================');
+    if (!channel) return;
+    channel.bind(`quotes.update`, (response) => {
+      console.log(
+        "============================ORDER UPDATE============================"
+      );
       console.log(response);
-      setOrders((orders)=>{
+      setOrders((orders) => {
         const data = response;
-        
+
         const newOrders = [...orders];
-        data.orders.forEach((order, i) => {
+        console.log(data);
+        data.quotes.forEach((order, i) => {
           const orderChange = order;
-          const orderIndex = orders.findIndex(order => order.id === orderChange.id);
+          const orderIndex = orders.findIndex(
+            (order) => order.id === orderChange.id
+          );
           //Need a check for a new order, sort by the status key on update
-          if(orderIndex !== -1) {
+          if (orderIndex !== -1) {
             orderChange.quote = newOrders[orderIndex].quote;
-            newOrders[orderIndex] = {...newOrders[orderIndex], ...orderChange};
+            newOrders[orderIndex] = {
+              ...newOrders[orderIndex],
+              ...orderChange,
+            };
           }
         });
         const sorted = sortAndOrder(newOrders, status_options);
-  
+
         return sorted;
       });
     });
-
   }, [channel, sortAndOrder, status_options]);
-  
-  if(!channel) return null;
-  
-  
 
-  
+  if (!channel) return null;
 
   return (
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <div className="grid grid-cols-3 gap-0 w-full grid-container">
-        <StatusSection color='bg-emerald-50' orders={orders} statusKey={'OD'} statusOptions={status_options} />
-        <StatusSection color='bg-emerald-200' orders={orders} statusKey={'IP'} statusOptions={status_options} />
-        <StatusSection color='bg-emerald-400' orders={orders} statusKey={'PN'} statusOptions={status_options} />
-        <StatusSection color='bg-emerald-100' orders={orders} statusKey={'DL'} statusOptions={status_options} />
-        <StatusSection color='bg-emerald-300' orders={orders} statusKey={'IR'} statusOptions={status_options} />
-        <StatusSection color='bg-emerald-500' orders={orders} statusKey={'AW'} statusOptions={status_options} />
+      <div className="grid-container grid w-3/4 grid-cols-3 gap-0">
+        <StatusSection
+          color="bg-emerald-50"
+          orders={orders}
+          statusKey={"OD"}
+          statusOptions={status_options}
+        />
+        <StatusSection
+          color="bg-emerald-200"
+          orders={orders}
+          statusKey={"IP"}
+          statusOptions={status_options}
+        />
+        <StatusSection
+          color="bg-emerald-400"
+          orders={orders}
+          statusKey={"PN"}
+          statusOptions={status_options}
+        />
+        <StatusSection
+          color="bg-emerald-100"
+          orders={orders}
+          statusKey={"DL"}
+          statusOptions={status_options}
+        />
+        <StatusSection
+          color="bg-emerald-300"
+          orders={orders}
+          statusKey={"IR"}
+          statusOptions={status_options}
+        />
+        <StatusSection
+          color="bg-emerald-500"
+          orders={orders}
+          statusKey={"AW"}
+          statusOptions={status_options}
+        />
+      </div>
+      <div className="h-screen w-1/4">
+        <StatusSection
+          color="bg-sky-100"
+          fullHeight={true}
+          orders={custom}
+          isCustom
+          titleOverride={"Custom Orders"}
+          statusOptions={status_options}
+        />
       </div>
     </main>
   );
